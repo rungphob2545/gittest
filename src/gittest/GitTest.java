cf8643ad7dbc1f20c002ee5652a6e9d660953f74
rungphob <ploypanista1122@gmail.com>
2020-10-20 15:12:19 +0700

    First Commit

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,12 @@
+
+package gittest;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+}
